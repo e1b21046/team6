5f7181812f75f8e90048d6b024d6e7ea92fffd42
e1b21046 <e1b21046@oit.ac.jp>
2023-10-03 16:21:13 +0900

    sample2-3の実装

--- src/main/java/oit/is/team6/lec02/team6/controller/Sample21Controller.java
+++ src/main/java/oit/is/team6/lec02/team6/controller/Sample21Controller.java
@@ -7,17 +7,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-/**
- * Sample21Controller
- *
- * クラスの前に@Controllerをつけていると，HTTPリクエスト（GET/POSTなど）があったときに，このクラスが呼び出される
- */
 @Controller
 public class Sample21Controller {
 
-  /**
-   * sample21というGETリクエストがあったら sample21()を呼び出し，sample21.htmlを返す
-   */
   @GetMapping("/sample21")
   public String sample21() {
     return "sample21.html";
@@ -41,15 +33,20 @@ public class Sample21Controller {
    */
   @GetMapping("/sample22/{param1}/{param2}")
   public String sample22(@PathVariable String param1, @PathVariable String param2, ModelMap model) {
-    int tasu = Integer.parseInt(param1);// param1が文字列なので，parseIntでint型の数値に変換する
+    int tasu = Integer.parseInt(param1);
     int tasareru = Integer.parseInt(param2);
     int tasuResult = tasu + tasareru;
 
-    // ModelMap型変数のmodelにtasuResult1という名前の変数で，tasuResultの値を登録する．
-    // ここで値を登録するとthymeleafが受け取り，htmlで処理することができるようになる
     model.addAttribute("tasuResult1", tasuResult);
     return "sample21.html";
 
+  }
+
+  @GetMapping("/sample23")
+  public String sample23(@RequestParam Integer tasu1, @RequestParam Integer tasu2, ModelMap model) {
+    int tasuResult = tasu1 + tasu2;
+    model.addAttribute("tasuResult2", tasuResult);
+    return "sample21.html";
   }
 
 }
